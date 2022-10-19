1f8861dedd82dd497f9592be0269a0497982d8d3
Mudit Pathak <116190802+MuditPandit20@users.noreply.github.com>
2022-10-19 22:59:30 +0530

    Add files via upload

---
+++ collage name.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("My name is Mudit Pathak\n I am Study in GLA University\n I am BCA Student\n");
+    
+}
---
+++ main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    //printf("Hello World\n");
+    printf("Mudit Pathak");
+    return 0;
+
+}
